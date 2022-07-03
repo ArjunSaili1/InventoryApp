e69558fa8b584f2303570cf84345ed826cf62207
Arjun Saili <arjun7.saili@gmail.com>
2022-07-03 00:53:51 -0400

    Implement update cuisine

--- controllers/cuisineController.js
+++ controllers/cuisineController.js
@@ -1,102 +1,150 @@
-const Cuisine = require('../models/cuisine')
-const Meal = require('../models/meal')
-const { body,validationResult } = require('express-validator');
-const cloudinary = require('../utils/cloudinary');
+const Cuisine = require("../models/cuisine");
+const Meal = require("../models/meal");
+const { body, validationResult } = require("express-validator");
+const cloudinary = require("../utils/cloudinary");
 
+module.exports.cuisine_list = async function (req, res, next) {
+  try {
+    const allCuisines = await Cuisine.find({}).exec();
+    res.render("grid", { items: allCuisines, itemType: "Cuisine" });
+  } catch (err) {
+    return next(err);
+  }
+};
 
-module.exports.cuisine_list = async function(req, res, next){
-    try{
-        const allCuisines = await Cuisine.find({}).exec();
-        res.render("grid", {items: allCuisines, itemType: "Cuisine"})
-    }catch(err){
-        return next(err)
-    }
-}
+module.exports.cuisine_detail = async function (req, res, next) {
+  try {
+    const cuisine = await Cuisine.findById(req.params.id).exec();
+    const cuisineMeals = await Meal.find({ cuisine: cuisine._id }).exec();
+    res.render("cuisine_detail", {
+      cuisine: cuisine,
+      cuisineMeals: cuisineMeals,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
 
-module.exports.cuisine_detail = async function(req, res, next){
-    try{
-        const cuisine = await Cuisine.findById(req.params.id).exec();
-        const cuisineMeals = await Meal.find({cuisine: cuisine._id}).exec()
-        res.render('cuisine_detail', {cuisine: cuisine, cuisineMeals: cuisineMeals})
-    }catch(err){
-        return next(err)
-    }
-}
-
-module.exports.cuisine_create_get = function(req, res, next){
-    res.render('cuisine_form', {title: "Create Cuisine"})
-}
+module.exports.cuisine_create_get = function (req, res, next) {
+  res.render("cuisine_form", { title: "Create Cuisine" });
+};
 
 module.exports.cuisine_create_post = [
-    body("cuisine_name", "Cuisine Name is Required.").trim().isLength({min: 1}).escape(),
-    body("cuisine_description", "Cuisine description is required.").isLength({min: 1}).escape(),
-    async function(req, res, next){
-        const validation = validationResult(req);
-        if(!validation.isEmpty()){
-            res.render("cuisine_form", {title: "Create Cuisine", errors: validation["errors"]})
-            return;
-        }
-        try{
-            let newCuisine = new Cuisine({
-                name: req.body.cuisine_name,
-                description: req.body.cuisine_description
-            })
-            if(req.file){
-                const result = await cloudinary.uploader.upload(req.file.path,{
-                    resource_type: "image",
-                    public_id: `cuisine/${newCuisine._id}`
-                })
-                newCuisine.image = result.url
-            }
-            await newCuisine.save()
-            res.redirect(`/cuisine/${newCuisine._id}`)
-        }catch(err){
-            return next(err)
-        }
-}]
-
-module.exports.cuisine_update_get = async function(req, res, next){
-    try{
-        const cuisine = await Cuisine.findById(req.params.id);
-        res.render('cuisine_form', {title: "Update Cuisine", cuisine: cuisine})
-    }catch(err){
-        return next(err)
+  body("cuisine_name", "Cuisine Name is Required.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("cuisine_description", "Cuisine description is required.")
+    .isLength({ min: 1 })
+    .escape(),
+  async function (req, res, next) {
+    const validation = validationResult(req);
+    if (!validation.isEmpty()) {
+      res.render("cuisine_form", {
+        title: "Create Cuisine",
+        errors: validation["errors"],
+      });
+      return;
     }
-}
-
-module.exports.cuisine_update_post = function(req, res, next){
-    res.send("Not Implemented Yet: Update Cuisine ID:" + JSON.stringify(req.params.id))
-}
-
-module.exports.cuisine_delete_get = async function(req, res, next){
     try {
-        const cuisine = await Cuisine.findById(req.params.id);
-        const cuisineMeals = await Meal.find({
-          cuisine: req.params.id,
+      let newCuisine = new Cuisine({
+        name: req.body.cuisine_name,
+        description: req.body.cuisine_description,
+      });
+      if (req.file) {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          resource_type: "image",
+          public_id: `cuisine/${newCuisine._id}`,
         });
-        res.render("delete_cuisine", {
-          cuisine,
-          deleteLink: req.originalUrl,
-          cuisineMeals,
-        });
-      } catch (err) {
-        return next(err);
+        newCuisine.image = result.url;
       }
-}
+      await newCuisine.save();
+      res.redirect(`/cuisine/${newCuisine._id}`);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
 
-module.exports.cuisine_delete_post = async function(req, res, next){
+module.exports.cuisine_update_get = async function (req, res, next) {
+  try {
+    const cuisine = await Cuisine.findById(req.params.id);
+    res.render("cuisine_form", { title: "Update Cuisine", cuisine: cuisine });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports.cuisine_update_post = [
+  body("cuisine_name", "Cuisine Name is Required.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("cuisine_description", "Cuisine description is required.")
+    .isLength({ min: 1 })
+    .escape(),
+
+  async function (req, res, next) {
+    const cuisine = await Cuisine.findById(req.params.id);
+    const validation = validationResult(req);
+    if (!validation.isEmpty()) {
+      res.render("cuisine_form", {
+        title: "Update Cuisine",
+        cuisine: cuisine,
+        errors: validation["errors"],
+      });
+      return;
+    }
     try {
-        const cuisineMeals = await Meal.find({
-          cuisine: req.params.id,
-        });
-        if(cuisineMeals.length === 0){
-          await Cuisine.deleteOne({ _id: req.params.id });
-          res.redirect("/cuisine");
-        }
-        else{
-          res.redirect(`/cuisine/${req.params.id}`)
-        }
-      } catch (err) {
-        return next(err);
+      if (cuisine.title !== req.body.cuisine_name) {
+        cuisine.name = req.body.cuisine_name;
       }
-}
+      if (cuisine.description !== req.body.cuisine_description) {
+        cuisine.description = req.body.cuisine_description;
+      }
+      if (req.file) {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          resource_type: "image",
+          public_id: `cuisine/${cuisine._id}`,
+        });
+        cuisine.image = result.url;
+      }
+      await cuisine.save();
+      res.redirect(`/cuisine/${cuisine._id}`);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
+
+module.exports.cuisine_delete_get = async function (req, res, next) {
+  try {
+    const cuisine = await Cuisine.findById(req.params.id);
+    const cuisineMeals = await Meal.find({
+      cuisine: req.params.id,
+    });
+    res.render("delete_cuisine", {
+      cuisine,
+      deleteLink: req.originalUrl,
+      cuisineMeals,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports.cuisine_delete_post = async function (req, res, next) {
+  try {
+    const cuisineMeals = await Meal.find({
+      cuisine: req.params.id,
+    });
+    if (cuisineMeals.length === 0) {
+      await Cuisine.deleteOne({ _id: req.params.id });
+      res.redirect("/cuisine");
+    } else {
+      res.redirect(`/cuisine/${req.params.id}`);
+    }
+  } catch (err) {
+    return next(err);
+  }
+};
--- routes/cuisineRouter.js
+++ routes/cuisineRouter.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const upload = require('../utils/multer');
-const cuisine_controller = require('../controllers/cuisineController')
+const upload = require("../utils/multer");
+const cuisine_controller = require("../controllers/cuisineController");
 
-router.get('/', cuisine_controller.cuisine_list)
+router.get("/", cuisine_controller.cuisine_list);
 
-router.get('/create', cuisine_controller.cuisine_create_get)
+router.get("/create", cuisine_controller.cuisine_create_get);
 
-router.post('/create', upload.single("cuisine_img"), cuisine_controller.cuisine_create_post)
+router.post(
+  "/create",
+  upload.single("cuisine_img"),
+  cuisine_controller.cuisine_create_post
+);
 
-router.get('/:id/update', cuisine_controller.cuisine_update_get)
+router.get("/:id/update", cuisine_controller.cuisine_update_get);
 
-router.post('/:id/update', cuisine_controller.cuisine_update_post)
+router.post(
+  "/:id/update",
+  upload.single("cuisine_img"),
+  cuisine_controller.cuisine_update_post
+);
 
-router.get('/:id/delete', cuisine_controller.cuisine_delete_get)
+router.get("/:id/delete", cuisine_controller.cuisine_delete_get);
 
-router.post('/:id/delete', cuisine_controller.cuisine_delete_post)
+router.post("/:id/delete", cuisine_controller.cuisine_delete_post);
 
-router.get('/:id', cuisine_controller.cuisine_detail)
+router.get("/:id", cuisine_controller.cuisine_detail);
 
 module.exports = router;
