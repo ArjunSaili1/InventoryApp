6d63f9f6cf27bde321a8eb5a4250a5b9c8456cc0
Arjun Saili <arjun7.saili@gmail.com>
2022-07-16 02:26:49 -0400

    lint/prettier

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser')
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var mealRouter = require('./routes/mealRouter');
-var cuisineRouter = require('./routes/cuisineRouter');
-var ingredientRouter = require('./routes/ingredientRouter')
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var mealRouter = require("./routes/mealRouter");
+var cuisineRouter = require("./routes/cuisineRouter");
+var ingredientRouter = require("./routes/ingredientRouter");
 var app = express();
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', indexRouter);
-app.use('/meal', mealRouter);
-app.use('/cuisine', cuisineRouter);
-app.use('/ingredient', ingredientRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/meal", mealRouter);
+app.use("/cuisine", cuisineRouter);
+app.use("/ingredient", ingredientRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
